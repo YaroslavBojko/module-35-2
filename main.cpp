c54ad8cda90a1a5b1a64ed4cae2100b222df7544
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-10-14 19:35:29 +0300

    end

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <memory>
+
+void print(std::vector<int> &vector)
+{
+    for (auto &i: vector)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<int> numbers =  {1, 3, 5, 1, 7, 2, 3, 4, 1, 6};
+    print(numbers);
+
+    auto lambda = [](std::vector<int>& v)
+    {
+        auto values = new std::unordered_set<int>();
+        auto result = std::make_unique<std::vector<int>>();
+        for (auto &i : v) {
+            if (values->insert(i).second)
+                result->push_back(i);
+        }
+        return result;
+    };
+
+    auto vector = lambda(numbers);
+    print(*vector);
+
 }
